a65434827514e33dd82a24fce40311b2c4fcad18
Maxo <65329519+MaxoRuiz@users.noreply.github.com>
2020-06-08 08:13:28 -0500

    Delete Actividad2.php

--- Actividad2.php
+++
@@ -1,9 +0,0 @@
-<?php
-  echo "Ruiz Gonzalez Julio Maximiliano<br>
-          Cosas que me han gustado:<br>
-            1-HTML<br>
-            2-php<br>
-          Cosas que relamente disfruto:<br>
-            1- La paciencia que nos tienen los instructores<br>
-            2- Que se tomen el tiempo de explicar y enseñar cada tema";
- ?>
